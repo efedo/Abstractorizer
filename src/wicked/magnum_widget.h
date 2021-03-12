42a176fef1affefbed93a953f5c8d6ce40972784
Eric Fedosejevs <eric.fedosejevs@gmail.com>
2021-03-12 09:15:35 -0600

    misc

---
+++ src/wicked/magnum_widget.h
@@ -0,0 +1,19 @@
+#ifndef MAGNUM_WIDGET_H
+#define MAGNUM_WIDGET_H
+
+#include "../generic/engine_widget.h"
+
+
+class MagnumWidgetInternal;
+
+class MagnumWidget : public EngineWidget {
+public:
+	MagnumWidget(QWidget* parent);
+	~MagnumWidget();
+	void exposeEvent(bool visible = true) override;
+	void resizeEvent(QResizeEvent*) override;
+private:
+	MagnumWidgetInternal * internalWidgetPtr = 0;
+};
+
+#endif //MAGNUM_WIDGET_H
